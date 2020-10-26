ede27acde0fbe54acc498a8f6f6b745d718087fd
Uzeen Chhabra <uzeenc@amdocs.com>
2020-10-26 21:12:48 +0530

    initial commit

---
+++ src/test/java/com/springDI/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDI.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
